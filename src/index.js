25b0a2443fe6e4d1b8259b0a1e856a0c357105de
webskydavid <webskydavid@gmail.com>
2019-10-23 15:37:11 +0200

    Added CRUD functions

--- src/index.js
+++ src/index.js
@@ -2,43 +2,205 @@ import React, { useState, useEffect, createContext, useContext } from 'react';
 import ReactDOM from 'react-dom';
 const Context = createContext();
 
+const initData = {
+  items: [
+    {
+      id: 'fwefjhk38r432jf9',
+      name: 'Github',
+      dateTime: '2019-10-20',
+      login: 'test',
+      password: 'testing'
+    },
+    {
+      id: 'fwefjhk38r43fff9',
+      name: 'Netflix',
+      dateTime: '2019-10-20',
+      login: 'test',
+      password: 'testing'
+    }
+  ]
+};
+
+const DataProvider = ({ children }) => {
+  // Dummy data
+  const [state, setState] = useState(initData);
+  const [itemToUpdate, setItemToUpdate] = useState(false);
+
+  const create = item => {
+    setState(s => {
+      return {
+        ...s,
+        items: [...s.items, item]
+      };
+    });
+  };
+  const update = (id, payload) => {
+    const index = state.items.indexOf(i => i.id === id);
+    const item = state.items[index];
+    setState(s => ({
+      ...s,
+      items: [...s.items]
+    }));
+  };
+  const read = () => {
+    return state.items;
+  };
+
+  const remove = () => {};
+
+  console.log(itemToUpdate);
+
+  return (
+    <Context.Provider
+      value={{
+        create,
+        update,
+        read,
+        remove,
+        itemToUpdate,
+        setItemToUpdate
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
 const List = () => {
-  const context = useContext(Context);
+  const { read, setItemToUpdate, itemToUpdate } = useContext(Context);
+  const [show, setShow] = useState();
+
+  const handleShow = id => {
+    if (show === id) {
+      setShow(undefined);
+    } else {
+      setShow(id);
+    }
+  };
+
   return (
     <ul>
-      <li>
-        Github - 19.10.2019 <button>Show</button>
-        <button>Edit</button>
-      </li>
-      <li>Gmail</li>
-      <li>Secret note</li>
-      <li>Hotmail</li>
+      {read().map(item => {
+        return (
+          <li key={item.id}>
+            {item.name} - {item.dateTime}
+            {show && show === item.id && item.password + ' ' + item.login}
+            <button onClick={() => handleShow(item.id)}>Show</button>
+            <button onClick={() => setItemToUpdate(item)}>Edit</button>
+            {itemToUpdate.id === item.id ? 'Updating!' : ''}
+          </li>
+        );
+      })}
     </ul>
   );
 };
 
-const Add = () => {
+const formInitData = {
+  name: '',
+  login: '',
+  password: ''
+};
+
+const Form = () => {
+  // set to false
   const [toggle, setToggle] = useState(false);
+  const { create, update, itemToUpdate, setItemToUpdate } = useContext(Context);
+  console.log(itemToUpdate);
+
+  const [show, setShow] = useState({
+    login: false,
+    password: false
+  });
+  const [form, setForm] = useState(formInitData);
 
   useEffect(() => {
     if (!toggle) {
-      // reset form
+      setForm(formInitData);
+      setItemToUpdate(false);
     }
   }, [toggle]);
+
+  useEffect(() => {
+    if (itemToUpdate) {
+      setForm(itemToUpdate);
+      setToggle(true);
+    }
+  }, [itemToUpdate]);
+
+  const handleShow = field => {
+    setShow(s => ({ ...s, [field]: !s[field] }));
+  };
+
+  const handleChange = e => {
+    const {
+      target: { value, name }
+    } = e;
+
+    setForm(f => {
+      return {
+        ...f,
+        [name]: value
+      };
+    });
+  };
+
+  const handleCreate = () => {
+    if (itemToUpdate) {
+      update(id, {
+        name: form.name,
+        login: form.login,
+        password: form.password
+      });
+    } else {
+      create({
+        id: Date.now(),
+        name: form.name,
+        dateTime: '2019-10-20',
+        login: form.login,
+        password: form.password
+      });
+    }
+
+    setForm(formInitData);
+  };
 
   return (
     <>
       <button type="button" onClick={() => setToggle(t => !t)}>
-        Add new
+        {toggle ? 'Close' : 'Add new'}
       </button>
       <div style={{ display: toggle ? '' : 'none' }}>
-        <input name="name" placeholder="Name" />
+        <h4>Create or Update</h4>
+        <label>Name</label>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={form.name}
+          name="name"
+          placeholder="Name"
+        />
         <br />
-        <input name="login" placeholder="Login" />
+        <label>Login</label>
+        <input
+          type={show.login ? 'text' : 'password'}
+          onChange={handleChange}
+          value={form.login}
+          name="login"
+          placeholder="Login"
+        />
+        <button onClick={() => handleShow('login')}>Show</button>
         <br />
-        <input name="password" placeholder="password" />
+        <label>Password</label>
+        <input
+          type={show.password ? 'text' : 'password'}
+          onChange={handleChange}
+          value={form.password}
+          name="password"
+          placeholder="password"
+        />
+        <button onClick={() => handleShow('password')}>Show</button>
         <br />
-        <button>Save</button>
+        <button onClick={handleCreate}>Save</button>
       </div>
     </>
   );
@@ -46,11 +208,11 @@ const Add = () => {
 
 function App() {
   return (
-    <Context.Provider value={{}}>
+    <DataProvider>
       <List />
       <hr />
-      <Add />
-    </Context.Provider>
+      <Form />
+    </DataProvider>
   );
 }
 
